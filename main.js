60b11c3461a3a9119e6ac8acc1bb225a88317546
n0o0b090lv <ernestspursw@gmail.com>
2025-09-07 21:07:28 +0300

    SNOW :DD

--- main.js
+++ main.js
@@ -3,14 +3,41 @@ let diologue = document.getElementById("diologue")
 let backpack = document.getElementById("backpack")
 let backpack_img = document.getElementById("backpack_img")
 let inv_slots = document.getElementsByClassName("inv_slot")
+const timer = ms => new Promise(res => setTimeout(res, ms))
+
+
+class snowparticle {
+    constructor (ctx, spawn, velocity, radius) {
+        this.ctx = ctx
+        this.position = spawn
+        this.velocity = velocity
+        this.radius = radius
+    }
+    tick() {
+        if (this.position[0] > window.screen.width + this.radius) {
+            return false
+        }
+        if (this.position[1] > window.screen.height + this.radius) {
+            return false
+        }
+        drawCirle(this.ctx, this.position, this.radius)
+        this.position[0] += this.velocity[0]
+        this.position[1] += this.velocity[1]
+        this.velocity[0] += Math.random() * 2 -1
+        return true
+    }
+}
+
 
 let backpack_open = false
 let backpack_slots = [0,0,0,0,0,0]
 let held_slot = 0
 
+
 //init
 slotsInit()
 setTimeout(openPlace, 1000)
+snow()
 openDiologue()
 backpack.addEventListener('click', processBackpack)
 
@@ -80,3 +107,36 @@ function slotClicked () {
     held_slot = this.classList[1].slice(2);
 }
 //Snowstorm
+async function snow() {
+    const canvas = document.getElementById("snowstorm");
+    const ctx = canvas.getContext("2d");
+    canvas.width = window.screen.width
+    canvas.height = window.screen.height
+    let snow = []
+    const context = canvas.getContext('2d');
+    while (true) {
+        await timer(10)
+        let random_x = Math.random() * window.screen.width - window.screen.width
+        let random_Y = Math.random() * window.screen.height - window.screen.height
+        let random_speed_x = Math.random() * 10 + 1
+        let random_speed_y = Math.random() * 10 + 1
+        context.clearRect(0, 0, canvas.width, canvas.height);
+        snow.push(new snowparticle(ctx, [random_x,random_Y], [random_speed_x,random_speed_y], Math.random()*20+10))  
+        for (let index = 0; index < snow.length; index++) {
+            const element = snow[index];
+            if (!element.tick() && index == 0) {
+                snow.shift()
+                index -= 1
+            }
+        }
+    }
+}
+function drawCirle(ctx, pos, radius) {
+    ctx.beginPath();
+    ctx.arc(pos[0], pos[1], radius, 0, 2 * Math.PI);
+    ctx.fillStyle = "white";
+    ctx.fill();
+    ctx.lineWidth = 1;
+    ctx.strokeStyle = "gray";
+    ctx.stroke();
+}
