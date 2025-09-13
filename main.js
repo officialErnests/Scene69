0c66b058492ba24c541da0c287865d38d964da71
n0o0b090lv <ernestspursw@gmail.com>
2025-09-13 23:17:14 +0300

    Math ;-;

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ let backpack = document.getElementById("backpack")
 let backpack_img = document.getElementById("backpack_img")
 let inv_slots = document.getElementsByClassName("inv_slot")
 const timer = ms => new Promise(res => setTimeout(res, ms))
+const img_assets_1 = new Image(192, 128)
+img_assets_1.src = "xcf/V1_SnowAssets_1.png"
 
 
 class snowparticle {
@@ -12,18 +14,44 @@ class snowparticle {
         this.position = spawn
         this.velocity = velocity
         this.radius = radius
+        this.stage = 0
+        this.progressTimer = Math.random() * 100
+        this.sizeVel = Math.random() * 0.003
+        this.acelar = Math.random() * 0.1 - 0.05
     }
     tick() {
-        if (this.position[0] > window.screen.width + this.radius) {
+        if (this.position[0] > 640 + this.radius) {
             return false
         }
-        if (this.position[1] > window.screen.height + this.radius) {
+        if (this.position[1] > window.innerHeight / window.innerWidth * 1000 + this.radius) {
             return false
         }
-        drawCirle(this.ctx, this.position, this.radius)
+        this.progressTimer -= 1
+        if (this.progressTimer <= 0) {
+            this.progressTimer = 5
+            switch (this.stage) {
+                case 0:
+                    this.stage = 1
+                    break;
+                case 1:
+                    this.stage = 2
+                    break;
+                case 2:
+                    this.stage = 3
+                    break;
+                case 3:
+                    this.stage = Math.floor(Math.random()*4+3) 
+                    break;
+                default:
+                    // this.stage = Math.floor(Math.random()*4+3) 
+                    break;
+            }
+        }
+        drawSnowball(this.ctx, this.position, this.radius, this.stage + 5)
         this.position[0] += this.velocity[0]
         this.position[1] += this.velocity[1]
-        this.velocity[0] += Math.random() * 2 -1
+        this.velocity[0] += this.acelar
+        this.radius += this.sizeVel
         return true
     }
 }
@@ -110,18 +138,26 @@ function slotClicked () {
 async function snow() {
     const canvas = document.getElementById("snowstorm");
     const ctx = canvas.getContext("2d");
-    canvas.width = window.screen.width
-    canvas.height = window.screen.height
     let snow = []
     const context = canvas.getContext('2d');
+    let timer2 = 0
     while (true) {
         await timer(10)
-        let random_x = Math.random() * window.screen.width - window.screen.width
-        let random_Y = Math.random() * window.screen.height - window.screen.height
-        let random_speed_x = Math.random() * 10 + 1
-        let random_speed_y = Math.random() * 10 + 1
-        context.clearRect(0, 0, canvas.width, canvas.height);
-        snow.push(new snowparticle(ctx, [random_x,random_Y], [random_speed_x,random_speed_y], Math.random()*20+10))  
+        timer2 += 0.01
+        // canvas.width = window.screen.width / window.screen.height * 1000
+        canvas.height = window.innerHeight / window.innerWidth * 1000
+        for (let index = 0; index < 100 - snow.length; index++) {
+            let random_x = Math.random() * 640 -640
+            let random_Y = Math.random() * window.innerHeight / window.innerWidth * 1000 - window.innerHeight / window.innerWidth * 1000
+            // let random_x = Math.random() * 640
+            // let random_Y = Math.random() * window.innerHeight / window.innerWidth * 1000
+            if (Math.floor(Math.random()*2))
+            let random_speed_x = Math.random() * 10 + 1
+            let random_speed_y = Math.random() * 5 + 2
+            // context.clearRect(0, 0, canvas.width, canvas.height);
+            ctx.globalAlpha = Math.sin(timer2)/4+0.7;
+            snow.unshift(new snowparticle(ctx, [random_x,random_Y], [random_speed_x,random_speed_y], Math.random()*0.5+0.5))  
+        }
         for (let index = 0; index < snow.length; index++) {
             const element = snow[index];
             if (!element.tick() && index == 0) {
@@ -131,12 +167,54 @@ async function snow() {
         }
     }
 }
-function drawCirle(ctx, pos, radius) {
-    ctx.beginPath();
-    ctx.arc(pos[0], pos[1], radius, 0, 2 * Math.PI);
-    ctx.fillStyle = "white";
-    ctx.fill();
-    ctx.lineWidth = 1;
-    ctx.strokeStyle = "gray";
-    ctx.stroke();
+function drawSnowball(ctx, pos, radius, stage) {
+    var temp = getAsset(0,stage)
+    ctx.imageSmoothingEnabled = false;
+    ctx.drawImage(img_assets_1, temp[0], temp[1], temp[2], temp[3], pos[0], pos[1], 64 * radius, 64 * radius)
+    // ctx.beginPath();
+    // ctx.arc(pos[0], pos[1], radius, 0, 2 * Math.PI);
+    // ctx.fillStyle = "white";
+    // ctx.fill();
+    // ctx.lineWidth = 1;
+    // ctx.strokeStyle = "gray";
+    // ctx.stroke();
+}
+function getAsset(assetNum, id) {
+    switch (assetNum) {
+        case 0:
+            switch (id) {
+                case 0: //snow pile
+                    return [0,0,64,64]
+                case 1:
+                    return [64,0,64,64]
+                case 2:
+                    return [128,0,64,64]
+                case 3:
+                    return [192,0,64,64]
+                case 4:
+                    return [0,64,64,64]
+                case 5: // snow ball start
+                    return [64,64,32,32]
+                case 6:
+                    return [96,64,32,32]
+                case 7:
+                    return [128,64,32,32]
+                case 8:
+                    return [160,64,32,32]
+                case 9: // snow ball variations
+                    return [64,96,32,32]
+                case 10:
+                    return [96,96,32,32]
+                case 11:
+                    return [128,96,32,32]
+                case 12:
+                    return [160,96,32,32]
+                default:
+                    break;
+            }
+            break;
+    
+        default:
+            break;
+    }
 }
